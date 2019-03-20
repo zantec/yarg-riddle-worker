8a1e67999cfe1f016810100bdafa214c0f3b766f
zacharybanta <zachary.banta1994@gmail.com>
2019-03-20 10:31:26 -0500

    write temporary worker file

--- worker/index.js
+++ worker/index.js
@@ -3,6 +3,11 @@
 const _ = require('lodash');
 require('dotenv').config();
 
+var CronJob = require('cron').CronJob;
+
+new CronJob('* * * * * *', function() {
+  console.log('You will see this message every second');
+}, null, true, 'America/Los_Angeles');
 
 
 
