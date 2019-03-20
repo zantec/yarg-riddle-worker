9aaca3cabc28a37d7285e9cd9c9e40c7a543b960
zacharybanta <zachary.banta1994@gmail.com>
2019-03-20 14:11:11 -0500

    make cronjob handle sending post request every four hours

--- worker/index.js
+++ worker/index.js
@@ -1,13 +1,22 @@
 // main worker file
 
 const _ = require('lodash');
+const axios = require('axios');
 require('dotenv').config();
 
 var CronJob = require('cron').CronJob;
 
-new CronJob('* * * * * *', function() {
-  console.log('You will see this message every second');
-}, null, true, 'America/Los_Angeles');
+var count = 0;
 
+const job = new CronJob('* * */4 * * *', function() {
+  axios({
+    method: 'POST',
+    url: `${process.env.RIDDLE_API}/api/server/treasure`,
+  })
+    .catch((err) => {
+      // error handling is for noobs
+    });
+}, null, true);
 
+job.start();
 
